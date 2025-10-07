c59670d4f62515b9346d71e98a9299fba1219a2a
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-10-07 07:30:49 +0100

    custom collector code has been added

---
+++ src/main/java/com/symphony/practise/CustomCollector.java
@@ -0,0 +1,18 @@
+package com.symphony.practise;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class CustomCollector {
+    static void main() {
+        List<String> data = List.of("apple", "banana", "cherry");
+        String result = data.stream().collect(
+                Collector.of(StringBuilder::new, //Supplier
+                        (x1, x2) -> x1.append(x2).append(", "), //Accumulator
+                        StringBuilder::append, //Combiner
+                        sb -> sb.substring(0, sb.length() - 2)) //Finisher
+        );
+        System.out.println(result);
+    }
+}
