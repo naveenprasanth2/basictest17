78ea293c6950627b4a5ec314c26103fcda2da632
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2026-01-03 09:00:02 +0000

    most frequent word 1 implementation

---
+++ src/main/java/com/symphony/practise1/MostFrequentWord1.java
@@ -0,0 +1,24 @@
+package com.symphony.practise1;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MostFrequentWord1 {
+    static void main() {
+        List<String> words = List.of("apple", "banana", "apple", "cherry", "banana", "banana", "apple");
+        List<String> result = words.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingLong(Map.Entry::getKey))
+                .map(Map.Entry::getValue)
+                .orElseThrow(NoSuchElementException::new);
+        System.out.println(!result.isEmpty()? result.getFirst() : "No Words Found");
+    }
+}
