6ea433a5be24b9b5cdb923da86160f33e447f3ea
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2026-01-25 12:40:23 +0000

    print even and odd in separate implementation with constraints

--- src/main/java/com/symphony/multi_threading/EvenAndOdd1.java
+++ src/main/java/com/symphony/multi_threading/EvenAndOdd1.java
@@ -2,7 +2,7 @@ package com.symphony.multi_threading;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class EvenAndOdd1 implements Runnable{
+public class EvenAndOdd1 implements Runnable {
     private final AtomicInteger count = new AtomicInteger(1);
     private final Object object;
 
@@ -12,24 +12,27 @@ public class EvenAndOdd1 implements Runnable{
 
     @Override
     public void run() {
-        while (count.get() <= 10) {
-            if (count.get() %2 == 0 && Thread.currentThread().getName().equals("even")) {
-                synchronized (object) {
-                    System.out.println("The value is " + count.get() + " and the thread name is " + Thread.currentThread().getName());
-                    count.getAndIncrement();
-                    try {
-                        object.wait();
-                    } catch (InterruptedException _) {
-                        throw new RuntimeException();
-                    }
+        while (true) {
+            synchronized (object) {
+                if (count.get() > 10) {
+                    object.notifyAll(); // Notify other threads before exiting
+                    break;
                 }
-            }
 
-            if (count.get() %2 != 0 && Thread.currentThread().getName().equals("odd")) {
-                synchronized (object) {
+                if (count.get() % 2 == 0 && Thread.currentThread().getName().equals("even")) {
                     System.out.println("The value is " + count.get() + " and the thread name is " + Thread.currentThread().getName());
                     count.getAndIncrement();
                     object.notify();
+                } else if (count.get() % 2 != 0 && Thread.currentThread().getName().equals("odd")) {
+                    System.out.println("The value is " + count.get() + " and the thread name is " + Thread.currentThread().getName());
+                    count.getAndIncrement();
+                    object.notify();
+                }
+
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
             }
         }
