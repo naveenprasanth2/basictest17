21c51532d6f7c1f9984bfebdcc8625c54bccd8d3
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2026-09-22 19:29:37 +0100

    MergeSort implementation

---
+++ src/main/java/com/symphony/practise3/MergeSort.java
@@ -0,0 +1,44 @@
+package com.symphony.practise3;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    static void main() {
+        int[] array = {1, 2, 1, 3, 4, 2, 3, 4, 5, 6, 5, 4, 5, 6, 5, 7, 8, 7, 8, 89};
+        System.out.println("Before Sorting");
+        System.out.println(Arrays.toString(array));
+        mergeSort(array);
+        System.out.println("After Sorting");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void mergeSort(int[] original) {
+        if (original.length < 2) return;
+        int len = original.length;
+        int mid = len / 2;
+        int[] left = new int[mid];
+        int[] right = new int[len - mid];
+        System.arraycopy(original, 0, left, 0, mid);
+        System.arraycopy(original, mid, right, 0, len - mid);
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, original);
+    }
+
+    private static void merge(int[] left, int[] right, int[] result) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+    }
+}
